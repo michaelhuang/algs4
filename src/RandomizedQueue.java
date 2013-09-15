fc5eee84cfe2d6f8ea40a6b3a7dc7cf7b1035280
hzf <michaelhuang12@gmail.com>
2013-09-15 21:21:27 +0800

    API

---
+++ src/RandomizedQueue.java
@@ -0,0 +1,40 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+	// construct an empty randomized queue
+	public RandomizedQueue() {
+	}
+
+	// is the queue empty?
+	public boolean isEmpty() {
+	}
+
+	// return the number of items on the queue
+	public int size() {
+	}
+
+	// add the item
+	public void enqueue(Item item) {
+	}
+
+	// delete and return a random item
+	public Item dequeue() {
+	}
+
+	// return (but do not delete) a random item
+	public Item sample() {
+	}
+
+	// return an independent iterator over items in random order
+	public Iterator<Item> iterator() {
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
