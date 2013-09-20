438d4c02fa79cd7a890d2b759d292eb5ee1a3081
hzf <michaelhuang12@gmail.com>
2013-09-20 21:39:34 +0800

    resolve backwash. all tests passed. :-)

--- src/Percolation.java
+++ src/Percolation.java
@@ -8,74 +8,86 @@
  */
 public class Percolation {
 
-	private WeightedQuickUnionUF quickUnionUF;
-	private int N;
-	private boolean[][] openness;
-	
-	// create N-by-N grid, with all sites blocked
-	public Percolation(int size) {
-		N = size;
-		openness = new boolean[N][N];
-		quickUnionUF = new WeightedQuickUnionUF(N*N + 2); // for two extra virtual sites
-	}
-	
-	// mapping 2D coordinates to 1D coordinates
-	private int xyTo1D(int i, int j) {
-		return (i-1)*N + j;
-	}
-	
-	// to check whether indices i,j are valid coordinates for the NxN matrix of sites
-	private boolean isValid(int i, int j) {
-		if (i <= 0 || i > N)
-			return false;
-		if (j <= 0 || j > N)
-			return false;
-		return true;
-	}
-	
-	// open site (row i, column j) if it is not already
+    private WeightedQuickUnionUF quickUnionUF;
+    private WeightedQuickUnionUF backWashUF;
+    private int N;
+    private boolean[][] openness;
+    
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int size) {
+        N = size;
+        openness = new boolean[N][N];
+        quickUnionUF = new WeightedQuickUnionUF(N*N + 2); // for two extra virtual sites
+        backWashUF = new WeightedQuickUnionUF(N*N + 1);   // for isFull()
+    }
+    
+    // mapping 2D coordinates to 1D coordinates
+    private int xyTo1D(int i, int j) {
+        return (i-1)*N + j;
+    }
+    
+    // to check whether indices i,j are valid coordinates for the NxN matrix of sites
+    private boolean isValid(int i, int j) {
+        if (i <= 0 || i > N)
+            return false;
+        if (j <= 0 || j > N)
+            return false;
+        return true;
+    }
+    
+    // open site (row i, column j) if it is not already
     public void open(int i, int j) throws Exception {
-    	if (isValid(i, j)) {
-    		if (!isOpen(i, j)) {
-    			openness[i-1][j-1] = true;
-    			if (isValid(i, j+1) && isOpen(i, j+1))
-    	    		quickUnionUF.union(xyTo1D(i, j), xyTo1D(i, j+1));
-    			if (isValid(i+1, j) && isOpen(i+1, j))
-    				quickUnionUF.union(xyTo1D(i, j), xyTo1D(i+1, j));
-    			if (isValid(i, j-1) && isOpen(i, j-1))
-    				quickUnionUF.union(xyTo1D(i, j), xyTo1D(i, j-1));
-    			if (isValid(i-1, j) && isOpen(i-1, j))
-    	    		quickUnionUF.union(xyTo1D(i, j), xyTo1D(i-1, j));
-    	    	
-    	    	if (i == 1)
-    	    		quickUnionUF.union(xyTo1D(i, j), 0);
-    	    	if ((i==N))
-    	    		quickUnionUF.union(xyTo1D(i, j), N*N+1); // got backwash
-    		}
-    	} else {
-    		throw new IndexOutOfBoundsException("wrong indices");
-    	}
+        if (isValid(i, j)) {
+            if (!isOpen(i, j)) {
+                openness[i-1][j-1] = true;
+                if (isValid(i, j+1) && isOpen(i, j+1)) {
+                	quickUnionUF.union(xyTo1D(i, j), xyTo1D(i, j+1));
+                	backWashUF.union(xyTo1D(i, j), xyTo1D(i, j+1));
+                }
+                if (isValid(i+1, j) && isOpen(i+1, j)) {
+                	quickUnionUF.union(xyTo1D(i, j), xyTo1D(i+1, j));
+                	backWashUF.union(xyTo1D(i, j), xyTo1D(i+1, j));
+                }
+                if (isValid(i, j-1) && isOpen(i, j-1)) {
+                	quickUnionUF.union(xyTo1D(i, j), xyTo1D(i, j-1));
+                	backWashUF.union(xyTo1D(i, j), xyTo1D(i, j-1));
+                }
+                if (isValid(i-1, j) && isOpen(i-1, j)) {
+                	quickUnionUF.union(xyTo1D(i, j), xyTo1D(i-1, j));
+                	backWashUF.union(xyTo1D(i, j), xyTo1D(i-1, j));
+                }
+                
+                if (i == 1) {
+                	quickUnionUF.union(xyTo1D(i, j), 0);
+                	backWashUF.union(xyTo1D(i, j), 0);
+                }
+                if ((i==N))
+                    quickUnionUF.union(xyTo1D(i, j), N*N+1); 
+            }
+        } else {
+            throw new IndexOutOfBoundsException("wrong indices");
+        }
     }
     
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) throws Exception {
-    	if (isValid(i, j))
-    		return openness[i-1][j-1];
-    	else
-    		throw new IndexOutOfBoundsException("wrong indices");
+        if (isValid(i, j))
+            return openness[i-1][j-1];
+        else
+            throw new IndexOutOfBoundsException("wrong indices");
     }
     
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) throws Exception {
-    	if (isValid(i, j))
-    		return quickUnionUF.connected(0, xyTo1D(i, j));
-    	else
-    		throw new IndexOutOfBoundsException("wrong indices");
+        if (isValid(i, j))
+            return backWashUF.connected(0, xyTo1D(i, j));
+        else
+            throw new IndexOutOfBoundsException("wrong indices");
     }
     
     // does the system percolate?
     public boolean percolates() {
-    	return quickUnionUF.connected(0, N*N+1);
+        return quickUnionUF.connected(0, N*N+1);
     }
     
     private void show() throws Exception {
@@ -94,13 +106,13 @@ public class Percolation {
             StdOut.print(quickUnionUF.connected(0, i) ? "1 " : "0 ");
         StdOut.println();
     }
-	
-	/**
-	 * @param args
-	 * @throws Exception 
-	 */
-	public static void main(String[] args) throws Exception {
-		In in = new In(args[0]);      // input file
+    
+    /**
+     * @param args
+     * @throws Exception 
+     */
+    public static void main(String[] args) throws Exception {
+        In in = new In(args[0]);      // input file
         int N = in.readInt();         // N-by-N percolation system
         // repeatedly read in sites to open and draw resulting system
         Percolation perc = new Percolation(N);
@@ -110,6 +122,6 @@ public class Percolation {
             perc.open(i, j);
         }
         perc.show();
-	}
+    }
 
 }
